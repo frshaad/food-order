184ed1bec6b2eb32b5590438826e5f96ab7a494c
Farshad Hatami <farshad.hatami95@gmail.com>
2023-08-30 18:55:41 +0330

    feat: firebase init

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center text-blue-600">
+      App
+    </div>
+  );
 }
